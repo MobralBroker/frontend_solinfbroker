d094e122766580bea643132ecf8152188c751e1a
jvlr9510 <jvitor.lr@hotmail.com>
2024-01-24 19:23:29 -0300

    Criando variaveis de ambiente para dockerfile

--- src/service/api.js
+++ src/service/api.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
 // const crudApi = axios.create({ baseURL: 'https://cqidt9eivj.loclx.io' });
-const authenticationApi = axios.create({ baseURL: 'http://localhost:8080' });
-const crudApi = axios.create({ baseURL: 'http://localhost:8081' });
+// const authenticationApi = axios.create({ baseURL: 'http://localhost:8080' });
+// const crudApi = axios.create({ baseURL: 'http://localhost:8081' });
+
+const authenticationApi = axios.create({ baseURL: process.env.REACT_APP_AUTH_API_URL });
+const crudApi = axios.create({ baseURL: process.env.REACT_APP_CRUD_API_URL });
+
 
 export { authenticationApi, crudApi };
