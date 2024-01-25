b1d2470ffd28af9b70f27b286910beed7688fad5
jvlr9510 <jvitor.lr@hotmail.com>
2024-01-25 00:16:48 -0300

    Corrigindo variavei de ambiente de REACT_APP para VUE_APP

--- src/service/api.js
+++ src/service/api.js
@@ -4,8 +4,8 @@ import axios from 'axios';
 // const authenticationApi = axios.create({ baseURL: 'http://localhost:8080' });
 // const crudApi = axios.create({ baseURL: 'http://localhost:8081' });
 
-const authenticationApi = axios.create({ baseURL: process.env.REACT_APP_AUTH_API_URL });
-const crudApi = axios.create({ baseURL: process.env.REACT_APP_CRUD_API_URL });
+const authenticationApi = axios.create({ baseURL: process.env.VUE_APP_AUTH_API_URL });
+const crudApi = axios.create({ baseURL: process.env.VUE_APP_CRUD_API_URL });
 
 
 export { authenticationApi, crudApi };
